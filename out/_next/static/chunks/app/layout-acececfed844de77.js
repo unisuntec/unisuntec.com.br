2913439e5ba33e63456c8d396922fb434ee5f59f
Guilherme Fialho <guilhermec.fialho@gmail.com>
2025-03-07 07:50:32 -0300

    Build for GitHub Pages: 2025-03-07 07:50:32

---
+++ out/_next/static/chunks/app/layout-acececfed844de77.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{991:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,5133,23)),Promise.resolve().then(l.t.bind(l,366,23))},366:()=>{},5133:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_d65c78"}}},e=>{var s=s=>e(e.s=s);e.O(0,[300,142,767,358],()=>s(991)),_N_E=e.O()}]);
